1bd22fd8982389403c3fc435b9b96fd72bee5164
Zach Schwartz <zachatorcodes@gmail.com>
2023-09-28 15:04:26 -0600

    Added more validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :trail
   belongs_to :user
+
+  validates :trail_id, :user_id, :rating, :comment, presence: true
 end
--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,4 +1,7 @@
 class Trail < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :users, through: :reviews
+
+  validates :trail_name, :length, :elevation, :location, :difficulty, presence: true
+  validates :trail_name, uniqueness: true
 end
