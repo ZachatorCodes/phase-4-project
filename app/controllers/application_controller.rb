db8e22021ac2bf4c85b34765b245cd6c5ea3b3ec
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-03 14:00:04 -0600

    Changed authentication method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,7 +16,8 @@ class ApplicationController < ActionController::API
   private
 
   def authorize
-    render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
+    @current_user = User.find_by(id: session[:user_id])
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @current_user
   end
 
   def render_unprocessable_entity_response(exception)
