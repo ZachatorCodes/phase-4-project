bd1cdd5b6a80e9881bb0c6bce87bea4c6a920852
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-05 17:49:41 -0600

    Wrap with UserProvider

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@
 import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import { UserProvider } from "./context/user";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -13,11 +14,13 @@ function App() {
   }, []);
 
   return (
-      <div className="App">
+    <div className="App">
+      <UserProvider>
         <Routes>
-          <Route exact path="/" element={ <Home /> }/>
+          <Route exact path="/" element={<Home />} />
         </Routes>
-      </div>
+      </UserProvider>
+    </div>
   );
 }
 
