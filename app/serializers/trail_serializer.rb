b0a8e69d2c40a56e1787fb0e114f071b9dd491dd
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-12 14:42:07 -0600

    Remove unnecessary attribute

--- app/serializers/trail_serializer.rb
+++ app/serializers/trail_serializer.rb
@@ -1,6 +1,7 @@
 class TrailSerializer < ActiveModel::Serializer
-  attributes :id, :trail_name, :length, :elevation, :location, :string, :difficulty
+  attributes :id, :trail_name, :length, :elevation, :location, :difficulty
 
   has_many :reviews
   has_many :users
 end
+ 
