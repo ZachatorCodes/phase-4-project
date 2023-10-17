ac9e80c4bf89c4f05364a6d9eee12ad65f9bd7cc
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-17 17:02:59 -0600

    Add signup and login components / routes

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import { UserProvider } from "./context/user";
+import Signup from "./components/Signup";
+import Login from "./components/Login";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,6 +20,8 @@ function App() {
       <UserProvider>
         <Routes>
           <Route exact path="/" element={<Home />} />
+          <Route exact path="/signup" element={<Signup />} />
+          <Route exact path="/login" element={<Login />} />
         </Routes>
       </UserProvider>
     </div>
