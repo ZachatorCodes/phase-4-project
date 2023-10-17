c234c83481da51bf03d50330be588ed5b07fbb5f
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-17 17:02:43 -0600

    Validate password and confirmation presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :trails, through: :reviews
 
-  validates :first_name, :last_name, :email, :username, :password, :bio, presence: true
+  validates :first_name, :last_name, :email, :username, :password, :bio, :password, :password_confirmation, presence: true
   validates :username, :email, uniqueness: true
 end
