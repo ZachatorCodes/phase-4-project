1782efd9683243c6ea3fc71c51c6223391fb9640
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-17 17:03:18 -0600

    Add user handling

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { UserContext } from "../context/user";
 
 function Home() {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
+  const { user } = useContext(UserContext);
+
+  if (!user || user.errors) {
+    return <h3>Please Signup or Login</h3>;
+  } else {
+    return (
+      <div>
+        <h3>
+          Welcome Home, {user.first_name} {user.last_name}
+        </h3>
+      </div>
+    );
+  }
 }
 
 export default Home;
