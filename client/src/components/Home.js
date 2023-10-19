ccc79aaea762ad887ec7b136b2c3041cb469e1a7
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-19 15:33:05 -0600

    Add trail component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,22 +2,27 @@ import React, { useContext } from "react";
 import { UserContext } from "../context/user";
 import { NavLink } from "react-router-dom";
 import Navbar from "./Navbar";
+import Trails from "./Trails";
 
 function Home() {
   const { user, loggedIn } = useContext(UserContext);
 
   if (loggedIn) {
     return (
-      <div className="logged-in-home">
-        <h3>
-          Welcome Home, {user.first_name} {user.last_name}
-        </h3>
+      <div className="home-background">
+        <Navbar />
+        <div className="logged-in-home">
+          <h3>
+          Welcome Home, {user.first_name} ☀️
+          </h3>
+          <Trails />
+        </div>
       </div>
     );
   } else {
     return (
       <div className="home-background">
-        <Navbar/>
+        <Navbar />
         <div className="logged-out-home">
           <div className="home-title">
             <h1>⛺ Welcome to BunkTrails 🥾</h1>
