03feeda48a040133f9d7d320e41b3c69f087a331
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-19 14:01:17 -0600

    Add show and strong params

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,10 +1,23 @@
 class TrailsController < ApplicationController
 
-  skip_before_action :authorize, only: :index
-
   def index
     trails = Trail.all
     render json: trails, status: :ok
+  end
+
+  def show
+
+  end
+
+  def create
+    trail = Trail.create!(trail_params)
+    render json: trail, status: :created
+  end
+
+  private
+
+  def trail_params
+    params.permit(:trail_name, :length, :elevation, :location, :difficulty)
   end
 
 end
