4380057956da6cb1d8e8186787731269df561a43
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-23 15:17:45 -0600

    Create trail card

--- client/src/components/Trails.js
+++ client/src/components/Trails.js
@@ -2,15 +2,31 @@ import React, { useContext } from "react";
 import { UserContext } from "../context/user";
 
 function Trails() {
-  const {trails} = useContext(UserContext)
+  const { trails } = useContext(UserContext);
 
   return (
-    <div className="Trails">
-      {trails.map(trail => {
-        return <h1 key={trail.id}>{trail.trail_name}</h1>
+    <div className="trails">
+      {trails.map((trail) => {
+        let trail_difficulty;
+        if (trail.difficulty === 3) {
+          trail_difficulty = "Hard";
+        } else if (trail.difficulty === 2) {
+          trail_difficulty = "Moderate";
+        } else {
+          trail_difficulty = "Easy";
+        }
+        return (
+          <div className="trail" key={trail.id}>
+            <h1 key={trail.id}>{trail.trail_name}</h1>
+            <p>Location: {trail.location}</p>
+            <p>Length: {trail.length} miles</p>
+            <p>Elevation Gain: {trail.elevation}</p>
+            <p>Difficulty: {trail_difficulty}</p>
+          </div>
+        );
       })}
     </div>
-  )
+  );
 }
 
 export default Trails;
