8b2f35c7fe40121e436ad1f0f6d3c24413e3a96e
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-25 13:51:45 -0600

    Add delete fetch handling

--- client/src/components/BuildReview.js
+++ client/src/components/BuildReview.js
@@ -2,16 +2,29 @@ import React, { useContext } from "react";
 import { UserContext } from "../context/user";
 import ReviewForm from "./ReviewForm";
 
-function BuildReview({ review }) {
+function BuildReview({ review, onDeleteReview }) {
   const { user } = useContext(UserContext);
   const userMatch = user.id === review.user_id;
+
+  function handleDelete() {
+    fetch(`/reviews/${review.id}`, {
+      method: "DELETE"
+    })
+    .then(r => {
+      if (r.ok) {
+        onDeleteReview(review)
+      }
+    })
+  }
+
   if (userMatch) {
     return (
       <div className="review">
         <h2>Review by: {user.username}</h2>
         <h2>{review.rating} / 10</h2>
         <p>💬 {review.comment}</p>
-        <button>Delete</button>
+        <button onClick={handleDelete}>Delete</button>
+        <button>Edit</button>
       </div>
     );
   } else {
