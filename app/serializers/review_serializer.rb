5ada7d8d0462058eb3cacc4f21e470340470eb51
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-25 15:59:56 -0600

    Add custom method for username

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,6 +1,10 @@
 class ReviewSerializer < ActiveModel::Serializer
-  attributes :id, :trail_id, :user_id, :rating, :comment
+  attributes :id, :trail_id, :user_id, :rating, :comment, :username
 
   belongs_to :user
   belongs_to :trail
+
+  def username
+    self.object.user.username
+  end
 end
