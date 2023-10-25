b3823ad21b12d16b8032a20c0d416a49d17b1efe
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-25 13:52:07 -0600

    Add destroy function

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,18 @@
 class ReviewsController < ApplicationController
+
   def create
     review = Review.create!(review_params)
     render json: review, status: :created
+  end
+
+  def update
+
+  end
+
+  def destroy
+    review = Review.find(params[:id])
+    review.destroy
+    head :no_content
   end
 
   private
