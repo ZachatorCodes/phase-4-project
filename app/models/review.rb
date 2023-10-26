d942aba126c196a928e65d79901c8746afbd2f6f
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-26 15:18:03 -0600

    Make it so users can only review a trail once

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,4 +4,5 @@ class Review < ApplicationRecord
 
   validates :trail_id, :user_id, :rating, :comment, presence: true
   validates :rating, numericality: { only_integer: true, less_than_or_equal_to: 10, greater_than_or_equal_to: 1}
+  validates :user_id, uniqueness: {scope: :trail_id, message: "can only review each trail once"}
 end
