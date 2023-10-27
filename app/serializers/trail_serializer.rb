023aa08046cefb072d8d77ecd83aa8cff18610cb
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-26 18:44:59 -0600

    Add custom method for number of reviews

--- app/serializers/trail_serializer.rb
+++ app/serializers/trail_serializer.rb
@@ -1,7 +1,11 @@
 class TrailSerializer < ActiveModel::Serializer
-  attributes :id, :trail_name, :length, :elevation, :location, :difficulty
+  attributes :id, :trail_name, :length, :elevation, :location, :difficulty, :number_of_reviews
 
   has_many :reviews
   has_many :users
+
+  def number_of_reviews
+    self.object.reviews.count
+  end
 end
  
