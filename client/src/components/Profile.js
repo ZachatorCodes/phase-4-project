ac9663e1e0b87cc8af2edd2bcf2889cfda243813
Zach Schwartz <zachatorcodes@gmail.com>
2023-10-26 18:43:29 -0600

    Show msg telling user to review trail if none

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,24 +1,43 @@
 import React, { useContext } from "react";
 import Navbar from "./Navbar";
 import { UserContext } from "../context/user";
+import Trails from "./Trails";
 
 function Profile() {
   const { user } = useContext(UserContext);
   console.log(user);
   if (user) {
-    return (
-      <div className="profile-background">
-        <Navbar />
-        <div className="profile-content">
-          <h2>
-            {user.first_name} {user.last_name}
-          </h2>
-          <p>
-            Username - {user.username} | Email - {user.email}
-          </p>
+    if (user.trails.length) {
+      return (
+        <div className="profile-background">
+          <Navbar />
+          <div className="profile-content">
+            <h2>
+              {user.first_name} {user.last_name}
+            </h2>
+            <p>
+              Username - {user.username} | Email - {user.email}
+            </p>
+            <Trails trails={user.trails} />
+          </div>
         </div>
-      </div>
-    );
+      );
+    } else {
+      return (
+        <div className="profile-background">
+          <Navbar />
+          <div className="profile-content">
+            <h2>
+              {user.first_name} {user.last_name}
+            </h2>
+            <p>
+              Username - {user.username} | Email - {user.email}
+            </p>
+            <h4>No reviewed trails. Get out there and review some trails!</h4>
+          </div>
+        </div>
+      );
+    }
   } else {
     return (
       <div className="profile-background">
@@ -27,7 +46,7 @@ function Profile() {
           <h1>Please Log In To View Your Profile</h1>
         </div>
       </div>
-    )
+    );
   }
 }
 
